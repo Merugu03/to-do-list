3710a856b075b7fe87757b459f42ea8ca80c362d
Merugu03 <meruguakshay@gmail.com>
2021-05-01 01:45:09 +0530

    created Task Component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,33 @@
-import React, { Component } from "react";
-class TodoList extends Component {
-  state = {};
-  render() {
-    return (
-      <div>
-        <div className="header text-center">
-          <h3>Todo List </h3>
-          <button className="btn btn-primary mt-3">Create Task</button>
-        </div>
-        <div className="task-container"></div>
+import React, { useState, Component } from "react";
+import CreateTask from "../popups/createTask";
+const TodoList = () => {
+  const [modal, setModal] = useState(false);
+  const [TaskList, setTaskList] = useState([]);
+  const toggle = () => {
+    setModal(!modal);
+  };
+  const saveTask = (taskobj) => {
+    let tempList = TaskList;
+    TaskList.push(taskobj);
+    setTaskList(tempList);
+    setModal(false);
+  };
+  return (
+    <div>
+      <div className="header text-center">
+        <h3>Todo List </h3>
+        <button className="btn btn-primary mt-3" onClick={() => setModal(true)}>
+          Create Task
+        </button>
       </div>
-    );
-  }
-}
+      <div className="task-container">
+        {TaskList.map((obj) => (
+          <li>{obj.Name}</li>
+        ))}
+      </div>
+      <CreateTask modal={modal} toggle={toggle} save={saveTask} />
+    </div>
+  );
+};
 
 export default TodoList;
